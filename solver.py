f587be7c76729c2fdade41856e1ab6c29904eba2
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-03 14:59:23 -0500

    Implement get_user_input to validate entries

--- solver.py
+++ solver.py
@@ -1,17 +1,31 @@
 def main():
     g = get_user_grid()
-    print(g)
+    for row in g:
+        print(row)
 
 
 def get_user_grid():
     rows = []
     for i in range(9):
         rows.append([])
-        print(rows[i]) 
         for j in range(9):
-            rows[i].append(input(f"Row {i+1}, Col {j+1}: "))
+            rows[i].append(get_user_input(i, j))
     return rows
 
+
+def get_user_input(i, j):
+    entry = input(f"Row {i+1}, Col {j+1}: ")
+
+    try:
+        number = int(entry)
+        if number >= 0 and number <= 9:
+            return number
+        else:
+            print("Must be an integer 0-9.")
+    except ValueError:
+        print("Invalid entry. Must be an integer 0-9.")
+
+    get_user_input(i, j)
 
 if __name__ == "__main__":
     main()
