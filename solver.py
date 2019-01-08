03b54d55bfa00df3f3aad80c8fbf012bef6113b4
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-07 19:57:51 -0500

    Implement check_horizontal - not fully working, see TODO

--- solver.py
+++ solver.py
@@ -11,11 +11,11 @@ class Grid():
     def print_grid(self):
         for row in self.rows:
             for value in row:
-                print(f"{value}\t", end='')
+                print(f"{value}", end='\t')
             print()
         print("---")
 
-    def add_value(self, value, row, col):
+    def put_value(self, value, row, col):
         self.rows[row-1][col-1] = value # TODO - Remove "-1", refactor
 
     def get_rows(self):
@@ -29,21 +29,22 @@ def main():
     g.print_grid()
 
     p = possibilities_init()
-    #  check_horizontal(g, p)
+    check_horizontal(g, p)
     p.print_grid()
 
 
 def check_horizontal(g, p):
-    rows = g.get_rows()
+    g_rows = g.get_rows()
+    p_rows = p.get_rows()
 
-    for i in range(len(rows)):
-        s = set(all_values).intersection(rows[i])
-        
-        for element in s:
-            all_values.remove(element)
-
-        for j in range(len(rows[i])):
-            p.add_value(all_values, i+1, j+1)
+    for i in range(len(g_rows)):
+        for j in range(len(g_rows[i])):
+            for k in range(len(p_rows[i])):
+                # TODO - Figure out what's wrong with line 45
+                print(p_rows[i][k])
+                if g_rows[i][j] in p_rows[i][k]:
+                    p.put_value(p_rows[i][k].remove(g_rows[i][j]), \
+                            i+1,j+1)
 
 
 def get_user_grid():
@@ -59,7 +60,7 @@ def get_user_grid():
             break
         else:
             u = get_user_input()
-            g.add_value(u['val'], u['row'], u['col'])
+            g.put_value(u['val'], u['row'], u['col'])
 
     return g
 
@@ -80,12 +81,13 @@ def get_user_input():
 
 def possibilities_init():
     p = Grid()
-    all_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     rows = p.get_rows()
 
     for i in range(len(rows)):
         for j in range(len(rows[i])):
-            p.add_value(all_values, i+1, j+1)
+            # Cannot create the list as a variable bc they'll all be 
+            # linked as references to that same variable.
+            p.put_value([1, 2, 3, 4, 5, 6, 7, 8, 9], i+1, j+1)
 
     return p
 
