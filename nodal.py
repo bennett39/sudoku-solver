1d7478ffbb98a94eae347d47ded1ce609d13ee4f
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-09 17:10:18 -0500

    Implement Node.__init__ with l/r/u/d directions

--- nodal.py
+++ nodal.py
@@ -1,8 +1,14 @@
 class Node():
-    def __init__(self, val, row, col):
-        self.val = val
+    def __init__(self, row, col):
         self.row = row
         self.col = col
+        self.right = None
+        self.left = None
+        self.up = None
+        self.down = None
+
+    def put_value(self, val):
+        self.val = val
 
 
 class Grid():
@@ -11,7 +17,8 @@ class Grid():
 
 
 def main():
-    n = Node(1, 2, 3)
+    n = Node(1, 1)
+    n.put_value(9)
     print(n.val)
     print(n.row)
     print(n.col)
