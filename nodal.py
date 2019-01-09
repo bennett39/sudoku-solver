5b7a89c2d24ba7760e74c83b1f20fbece5c8c74c
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-01-09 17:18:56 -0500

    Implement set_left, right, up, down, and box

--- nodal.py
+++ nodal.py
@@ -6,22 +6,48 @@ class Node():
         self.left = None
         self.up = None
         self.down = None
+        self.box = None
 
     def put_value(self, val):
         self.val = val
 
+    def set_left(self, left):
+        self.left = left
+
+    def set_right(self, right):
+        self.right = right
+
+    def set_up(self, up):
+        self.up = up
+
+    def set_box(self, box):
+        self.box = box
+
+    def set_down(self, down):
+        self.down = down
+
 
 class Grid():
     def __init__(self):
-        pass
+        for i in range(9):
+            for j in range(9):
+                pass
 
 
 def main():
-    n = Node(1, 1)
+    m = Node(1, 1)
+    m.put_value(8)
+
+    n = Node(2, 2)
     n.put_value(9)
+
+    n.set_left(m)
+    m.set_right(n)
+
     print(n.val)
-    print(n.row)
-    print(n.col)
+    print(n.left.val)
+    print(m.val)
+    print(m.right.val)
 
 
 if __name__ == "__main__":
